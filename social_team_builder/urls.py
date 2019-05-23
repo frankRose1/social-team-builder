e4893a9b03a6bda83d65f9102fb9908b8a0c7833
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-05-23 00:32:25 -0400

    Add signup logic and UserCreationForm

--- social_team_builder/urls.py
+++ social_team_builder/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'social_team_builder'
 
-url_patterns = [
+urlpatterns = [
   path('', views.index, name='home'),
   path('signup/', views.sign_up, name='sign_up'),
   path('signin/', views.sign_in, name='sign_in')
--- social_team_builder/views.py
+++ social_team_builder/views.py
@@ -1,4 +1,11 @@
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -6,8 +13,24 @@ def index(request):
 
 
 def sign_up(request):
-    return render(request, 'social_team_builder/signup.html')
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            # create the user and log them in
+            form.save()
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1']
+            )
+            login(request, user)
+            # success message here?
+            return HttpResponseRedirect(reverse('team_builder:home'))
+    return render(request, 'social_team_builder/signup.html', {'form': form})
 
 
 def sign_in(request):
-    return render(request, 'social_team_builder/signin.html')
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+    return render(request, 'social_team_builder/signin.html', {'form': form})
