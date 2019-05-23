8a9573a1ed90e53f07a6cdcef1dce1c08a9a62a0
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-05-22 23:51:12 -0400

    Add routes, templates, and views for signin and signup

--- social_team_builder/urls.py
+++ social_team_builder/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'social_team_builder'
 
 url_patterns = [
-  path('', views.index, name='home')
+  path('', views.index, name='home'),
+  path('signup/', views.sign_up, name='sign_up'),
+  path('signin/', views.sign_in, name='sign_in')
 ]
--- social_team_builder/views.py
+++ social_team_builder/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 def index(request):
     return render(request, 'social_team_builder/index.html')
+
+
+def sign_up(request):
+    return render(request, 'social_team_builder/signup.html')
+
+
+def sign_in(request):
+    return render(request, 'social_team_builder/signin.html')
