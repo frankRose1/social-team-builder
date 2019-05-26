4eb73d327a4abf53c54bb06d8afcd192ececd227
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-05-25 21:58:59 -0400

    Create the models

--- social_team_builder/models.py
+++ social_team_builder/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    """A user can create a profile with their details, skills, and avatar"""
+    pass
+
+
+class Project(models.Model):
+    """A user can create a project that they need help on, and other users can
+    apply to open positions on a project."""
+    title = models.CharField(max_length=255)
+    project_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_add_now=True)
+    published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class ProjectPosition(models.Model):
+    """A project position wil belong to a project and will have a name,
+      description and a related skill."""
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    skill = models.CharField(max_length=255)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    filled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectPositionApplicant(models.Model):
+    """Will model the relationship between a User and their relationship to a 
+    ProjectPosition"""
+    position = models.ForeignKey(ProjectPosition, on_delete=models.CASCADE)
+    applicant = models.ForeignKey(User, on_delete=models.CASCADE)
--- social_team_builder/views.py
+++ social_team_builder/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -67,3 +68,9 @@ def sign_out(request):
         'You were successfully signed out. Come back soon!'
     )
     return HttpResponseRedirect(reverse('team_builder:home'))
+
+
+# the user should be able to create a profile and upload an image
+@login_required
+def create_user_profile(request):
+    pass
